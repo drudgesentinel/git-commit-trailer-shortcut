7f4e74470ce1913614f7e7c3906a0983bb092295
Sean <sean.meininger@pm.me>
2021-01-18 23:59:35 -0800

    import config file

--- gct.js
+++ gct.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 const commander = require('commander');
 const program = new commander.Command();
+const config = require('./config');
 
 program
   .option('-m, --message', 'add commit message for the commit trailer');
 
 program.parse(process.argv);
+
+var config = {};
