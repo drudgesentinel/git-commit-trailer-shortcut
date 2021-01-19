6cbf7855334704ee25cedc791ff2bf0132826d37
Sean <sean.meininger@pm.me>
2021-01-19 00:20:35 -0800

    finish integrating exec

--- gct.js
+++ gct.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const exec = require('child_process');
 const commander = require('commander');
+const { stderr } = require('process');
 const program = new commander.Command();
 const config = require('./config');
 
@@ -13,3 +14,14 @@ const options = program.opts();
 
 const fullCommitMessage = `${options.message}\n\n${config.coAuthor1}`
 console.log(fullCommitMessage);
+exec(`git commit -m "${fullCommitMessage}"`, (error, stdout, stderr) => {
+  if(error) {
+    console.log(`Oh dear, this isn't supposed to happen. Error message: ${error.message}`);
+    return;
+  }
+  if(stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(stdout);
+});
