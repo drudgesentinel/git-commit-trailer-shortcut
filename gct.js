508462cb3039f427674de3183842f0e48d7b4477
Sean <sean.meininger@pm.me>
2021-01-19 00:27:00 -0800

    cowabunga
    Co-authored-by: Sean Meininger <test@super-programmer.com>

--- gct.js
+++ gct.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const exec = require('child_process');
+const {exec} = require('child_process');
 const commander = require('commander');
-const { stderr } = require('process');
+//const { stderr } = require('process');
 const program = new commander.Command();
 const config = require('./config');
 
@@ -13,8 +13,8 @@ program.parse(process.argv);
 const options = program.opts();
 
 const fullCommitMessage = `${options.message}\n\n${config.coAuthor1}`
-console.log(fullCommitMessage);
-exec(`git commit -m "${fullCommitMessage}"`, (error, stdout, stderr) => {
+//console.log(fullCommitMessage);
+exec(`/usr/bin/git add .; /usr/bin/git commit -m "${fullCommitMessage}"`, (error, stdout, stderr) => {
   if(error) {
     console.log(`Oh dear, this isn't supposed to happen. Error message: ${error.message}`);
     return;
